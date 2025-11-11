0da5b38c69f37a1327d3e82b81a5e7800ee3bf2d
nahdaauliya <nahdaaulia653@gmail.com>
2025-11-11 21:53:22 +0700

    heroSection and homepage

---
+++ resources/views/heroSection.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <section class="hero">
+        <div class="hero-text">
+            Laboratory of<br>
+            Data Technology
+        </div>
+    </section>
+
+    <section class="slider-container">
+        <div class="slider-track">
+            <img src="{{ asset('images/heroSection/1.jpg') }}" alt="">
+            <img src="{{ asset('images/heroSection/2.jpg') }}" alt="">
+            <img src="{{ asset('images/heroSection/3.jpg') }}" alt="">
+            <img src="{{ asset('images/heroSection/4.jpg') }}" alt="">
+
+            <img src="{{ asset('images/heroSection/1.jpg') }}" alt="">
+            <img src="{{ asset('images/heroSection/2.jpg') }}" alt="">
+            <img src="{{ asset('images/heroSection/3.jpg') }}" alt="">
+            <img src="{{ asset('images/heroSection/4.jpg') }}" alt="">
+        </div>
+    </section>
+</body>
+</html>
