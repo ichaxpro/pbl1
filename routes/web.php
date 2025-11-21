0ab593993c5daf8621e77d0bc3083d446a37aa4a
ichaxpro <dewiicha161@gmail.com>
2025-11-21 22:26:23 +0700

    add folder

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('home/homepage');
 });
 Route::get('/header', function () {
     return view('/header/header');
