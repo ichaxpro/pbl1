a6da019e3428516ec54ad8c29dce683c1500d442
M Bima J <bimamj7@gmail.com>
2025-12-05 09:50:34 +0700

    bracket

--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,7 @@ Route::get('/sidebar-operator', function () {
 
 Route::get('/sidebar-collapse', function () {
     return view('admin/sidebar_collapse');
+});
 Route::get('/news', function () {
     return view('news/news_page');
 });
