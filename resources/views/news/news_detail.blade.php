0eacf454fe10223fc9d2313744c5fc2ec9bd5268
diloo11 <dilawarharun27@gmail.com>
2025-12-09 19:12:38 +0700

    add feat news detail

---
+++ resources/views/news/news_detail.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Artikel Data Technology Laboratory</title>
+
+    <link rel="stylesheet" href="{{ asset('css/news_detail.css') }}">
+
+</head>
+
+<body>
+    <div class="container">
+        <h1>Data Technology Laboratory of POLINEMA Presents at Google Conference</h1>
+
+        <p class="date">4 November, 2025</p>
+
+        <figure>
+            <img src="https://placehold.co/800x450/2E8B57/FFFFFF?text=Technology+Conference+Image"
+                alt="Suasana Konferensi">
+
+            <figcaption>
+                Lorem Ipsum caption Suspendisse urna lacus, viverra et tincidunt in, aliquet quis arcu. In felis metus,
+                rutrum ac aliquam vel, pulvinar suscipit massa.
+            </figcaption>
+        </figure>
+
+        <h2>Sub Title 1</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet facilisis tortor. Suspendisse
+            urna lacus, viverra et tincidunt in, aliquet quis arcu. In felis metus, rutrum ac aliquam vel, pulvinar
+            suscipit massa. Fusce elit urna, mattis ac ultrices et, fermentum non massa. Nam quis justo metus. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet facilisis tortor. Suspendisse urna
+            lacus, viverra et tincidunt in, aliquet quis arcu.
+        </p>
+
+        <figure>
+            <img src="https://placehold.co/800x450/2E8B57/FFFFFF?text=Technology+Conference+Image"
+                alt="Suasana Konferensi">
+
+            <figcaption>
+                Lorem Ipsum caption Suspendisse urna lacus, viverra et tincidunt in, aliquet quis arcu. In felis metus,
+                rutrum ac aliquam vel, pulvinar suscipit massa.
+            </figcaption>
+        </figure>
+
+        <h2>Sub Title 1</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet facilisis tortor. Suspendisse
+            urna lacus, viverra et tincidunt in, aliquet quis arcu. In felis metus, rutrum ac aliquam vel, pulvinar
+            suscipit massa. Fusce elit urna, mattis ac ultrices et, fermentum non massa. Nam quis justo metus. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet facilisis tortor. Suspendisse urna
+            lacus, viverra et tincidunt in, aliquet quis arcu.
+        </p>
+    </div>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -151,3 +151,7 @@ Route::get('/user-management', function () {
 Route::get('/operator_gallery', function () {
     return view('operator/operator_gallery');
 });
+
+Route::get('/news_detail', function () {
+    return view('news/news_detail_page');   
+});
